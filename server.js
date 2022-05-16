f97702d55bf1d27c8ca9ffacd5fb85bfe70606d4
Jayden Cao <trex.icarus@gmail.com>
2022-05-17 01:01:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ console.log('server CONNECT')
 io.on('connection', socket => {
 	// const id = socket.handshake.query.id
 	// socket.join(id)
-	
+
 	socket.on('check-success', () => {
 		socket.emit('check-success-client', {
 			parent: {
 				title: 'Test song',
-				artist: 'Dumamay',
-				composer: 'Dumayma',
-				authorRight: 'Tao',
+				artist: 'Justin Bieber',
+				composer: 'Justin Beaver',
+				authorRight: 'Def Jam',
 				publisher: 'MCM',
 				image: 'https://cdn-icons.flaticon.com/png/512/2769/premium/2769339.png?token=exp=1652612636~hmac=6d8b9e1b061327683133b6327ab9b517',
 				phone: '0961254897',
